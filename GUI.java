8d40a677c2562cd45e9a0964a5e5bfab77ef6257
luckyeven <ssong094@uottawa.ca>
2020-08-01 20:28:53 -0400

    user interface

---
+++ GUI.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class GUI extends JFrame{
+
+	public void createGUI(){
+		JFrame frame = new JFrame(); // create JFrame object
+		frame.setBounds(600,400,600,300); //set window size
+		frame.setVisible(true);      // make JFrame visible
+		frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE); //release source when fram is closed
+		JLabel j1 = new JLabel("Keyword Cipher");
+
+	}
+
+}
