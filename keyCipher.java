c3357c57026335370db113e5b7a3eda633e36290
luckyeven <ssong094@uottawa.ca>
2020-08-04 04:05:30 -0400

    main function call for keycipher

---
+++ keyCipher.java
@@ -0,0 +1,17 @@
+import KeyWordCipher.*;
+
+
+/**
+* <h1>keyCipher</h1>
+* The keyCipher creat a GUI object
+*
+* @author shifeng song ssong094@uottawa.ca
+* @version 1
+* @since 2020-08-03
+*/
+public class keyCipher{
+	public static void main(String[] args) {
+		new GUI();
+	}
+
+}
